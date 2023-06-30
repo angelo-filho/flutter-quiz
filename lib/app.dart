613f92886a7b77b8f53621b11bcf6de0c005fd87
Angelo Filho <angelo.filho06@hotmail.com>
2023-06-30 16:24:26 -0300

    refactor: change my app widget to stateful

--- lib/app.dart
+++ lib/app.dart
@@ -2,9 +2,14 @@ import 'package:adv_basics/screens/questions_screen.dart';
 import 'package:adv_basics/screens/start_screen.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
